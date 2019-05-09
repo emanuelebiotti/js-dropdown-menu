464388e237096fd95be663e6c8ba4e8c0698e182
emanuele <emanuele.biotti@gmail.com>
2019-05-09 23:28:28 +0200

    Aggiunta considerazioni codice js

--- dropdown.js
+++ dropdown.js
@@ -1,7 +1,15 @@
-$('.menu-item').click(function() {
-  $(this).children('.dropdown-menu').removeClass('active');
-});
+// $('.menu-item').click(function() {
+//   $(this).children('.dropdown-menu').removeClass('active');
+// });
 
+// CONSIDERAZIONI:
 // Dovrei usare "toggle()" anziché "removeClass('active')", per fare sì che cliccando
 // una seconda volta il menu appena comparso possa poi scomparire.
 // Tuttavia usando "toggle" il menu, pur funzionando (appare e scompare), perde la formattazione che gli ho dato.
+
+
+// al momento uso toggle().
+
+$('.menu-item').click(function() {
+  $(this).children('.dropdown-menu').toggle();
+});
