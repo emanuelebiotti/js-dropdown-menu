fbd90c00d433ca708ebd4ee39d2316c1679a80db
emanuele <emanuele.biotti@gmail.com>
2019-05-09 23:17:54 +0200

    inserimento classe active e commenti in file js

--- dropdown.js
+++ dropdown.js
@@ -1,3 +1,7 @@
 $('.menu-item').click(function() {
-  $(this).children('.dropdown-menu').toggle();
+  $(this).children('.dropdown-menu').removeClass('active');
 });
+
+// Dovrei usare "toggle()" anziché "removeClass('active')", per fare sì che cliccando
+// una seconda volta il menu appena comparso possa poi scomparire.
+// Tuttavia usando "toggle" il menu, pur funzionando (appare e scompare), perde la formattazione che gli ho dato.
