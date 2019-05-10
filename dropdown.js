a5493f1ad9a3bef7b0bb62aa118ba9ff24d2d3ee
emanuele <emanuele.biotti@gmail.com>
2019-05-10 16:54:36 +0200

    aggiunta mouseenter

--- dropdown.js
+++ dropdown.js
@@ -2,14 +2,11 @@
 //   $(this).children('.dropdown-menu').removeClass('active');
 // });
 
-// CONSIDERAZIONI:
-// Dovrei usare "toggle()" anziché "removeClass('active')", per fare sì che cliccando
-// una seconda volta il menu appena comparso possa poi scomparire.
-// Tuttavia usando "toggle" il menu, pur funzionando (appare e scompare), perde la formattazione che gli ho dato.
 
+$('.menu-item').mouseenter(function() {
+  $(this).children('.dropdown-menu').removeClass('active');
+});
 
-// al momento uso toggle().
-
-$('.menu-item').click(function() {
-  $(this).children('.dropdown-menu').toggle();
+$('.menu-item').mouseleave(function() {
+  $(this).children('.dropdown-menu').addClass('active');
 });
